d18f60af624fee156ed545f0203c374c792628c3
gregoryput <gregorysanchez232@gmail.com>
2024-11-05 14:52:43 -0400

    correcion v2

--- src/layout/components/nav.jsx
+++ src/layout/components/nav.jsx
@@ -56,9 +56,9 @@ export default function Nav() {
             <TooltipTrigger asChild>
               <Button
                 variant="ghost"
-                className={`w-full text-gray-400 flex justify-center  ${rutaActual.pathname == "/Home" ? "text-blue-700 " : "group-hover:justify-start transition-all duration-300 ease-in-out"}`}
+                className={`w-full text-gray-400 flex justify-center  ${rutaActual.pathname == "/home" ? "text-blue-700 " : "group-hover:justify-start transition-all duration-300 ease-in-out"}`}
                 onClick={() => {
-                  navigate("/Home")
+                  navigate("/home")
                   setProject(0)
                 }}
               >
